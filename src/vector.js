1ec86271dccc26b5155b0e74872f3423cb6d6301
Bartosz Lorek <bery.lorek@gmail.com>
2017-05-26 16:30:10 +0200

    sort methods into groups, vectorArg handles multiple arguments

--- src/vector.js
+++ src/vector.js
@@ -5,12 +5,52 @@ export default function Vector(x, y, immutable) {
 }
 
 Vector.prototype = {
+    /**
+     *  Utility
+     */
     clone: function () {
         return new Vector(this.x, this.y);
     },
 
-    set: function (value) {
-        vectorArg(value, (x, y) => {
+    equals: function (vector) {
+        return this.x === vector.x && this.y === vector.y;
+    },
+
+    /**
+     *  Properties
+     */
+    get length() {
+        return Math.sqrt(this.dot(this));
+    },
+
+    set length(value) {
+        this.normalize().multiply(value);
+    },
+
+    get angle() {
+        return this.radians * 180 / Math.PI;
+    },
+
+    set angle(value) {
+        this.radians(value * Math.PI / 180);
+    },
+
+    get radians() {
+        return Math.atan2(this.y, this.x);
+    },
+
+    set radians(value) {
+        this.set(
+            Math.cos(value) * this.length,
+            Math.sin(value) * this.length
+        );
+    },
+
+    /**
+     *  Manipulation
+     */
+    set: function () {
+        vectorArg(arguments, (x, y) => {
             this.x = x;
             this.y = y;
         });
@@ -49,16 +89,42 @@ Vector.prototype = {
         return this;
     },
 
-    inverse: function () {
+    negate: function () {
         this.x *= -1;
         this.y *= -1;
         return this;
     },
 
     normalize: function (value = 1) {
-        return this.multiply(value / this.length);
+        this.multiply(value / this.length);
+        return this;
     },
 
+    limit: function (value) {
+        if (this.length > value) {
+            this.normalize(value);
+        }
+        return this;
+    },
+
+    magnitude: function (value) {
+        this.length = value;
+        return this;
+    },
+
+    rotate: function (angle) {
+        if (angle) {
+            const rad = (this.angle + angle) * Math.PI / 180;
+            const length = this.length;
+            this.x = Math.cos(rad) * length;
+            this.y = Math.sin(rad) * length;
+        }
+        return this;
+    },
+
+    /**
+     *  Products
+     */
     dot: function (vector) {
         return this.x * vector.x + this.y * vector.y;
     },
@@ -73,66 +139,14 @@ Vector.prototype = {
         return Math.sqrt((x * x) + (y * y));
     },
 
-    equals: function (vector) {
-        return this.x === vector.x && this.y === vector.y;
-    },
-
-    magnitude: function (value) {
-        this.length = value;
-        return this;
-    },
-
-    limit: function (value) {
-        if (this.length > value) {
-            this.normalize(value);
-        }
-        return this;
+    angleTo: function (vector) {
+        return this.radiansTo(vector) * 180 / Math.PI;
     },
 
     radiansTo: function (vector) {
         const length = this.length * vector.length;
         return length === 0 ? NaN : Math.acos(this.dot(vector) / length);
-    },
-
-    angleTo: function (vector) {
-        return this.radiansTo(vector) * 180 / Math.PI;
-    },
-
-    rotate: function (angle) {
-        if (angle !== 0) {
-            const rad = (this.angle + angle) * Math.PI / 180;
-            this.x = Math.cos(rad) * this.length;
-            this.y = Math.sin(rad) * this.length;
-        }
-        return this;
-    },
-
-    get length() {
-        return Math.sqrt(this.dot(this));
-    },
-
-    set length(value) {
-        this.normalize().multiply(value);
-    },
-
-    get radians() {
-        return Math.atan2(this.y, this.x);
-    },
-
-    set radians(value) {
-        this.set(
-            Math.cos(value) * this.length,
-            Math.sin(value) * this.length
-        );
-    },
-
-    get angle() {
-        return this.radians * 180 / Math.PI;
-    },
-
-    set angle(value) {
-        this.radians(value * Math.PI / 180);
-    },
+    }
 }
 
 Vector.random = function (a = 1, b = 1) {
@@ -140,13 +154,27 @@ Vector.random = function (a = 1, b = 1) {
 }
 
 function vectorArg(value, callback) {
-    if (value instanceof Vector) {
-        callback(value.x, value.y);
-    } else if (typeof value === 'number') {
+    if (typeof value === 'number') {
         callback(value, value);
+
+    } else if (value instanceof Vector) {
+        callback(value.x, value.y);
+
+    } else if (isArguments(value)) {
+        const x = value[0];
+        if (x !== undefined) {
+            const y = value[1] !== undefined
+                ? value[1]
+                : value[0];
+            callback(x, y);
+        }
     }
 }
 
 function isZero(vector) {
     return vector.x === 0 && vector.y === 0;
+}
+
+function isArguments(value) {
+    return value && value.hasOwnProperty('callee') || false;
 }
