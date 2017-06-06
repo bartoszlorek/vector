d907c2343ac1166071facb53f89d5a3b0f5bf58e
Bartosz Lorek <bery.lorek@gmail.com>
2017-06-06 18:23:57 +0200

    fastAtan2

--- test/performance/atan2.js
+++ test/performance/atan2.js
@@ -3,117 +3,79 @@ var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
 var x = random(-20, 20),
-    y = random(-10, 10);
+	y = random(-10, 10);
 
-// suite
-//     .add('Math.atan2', function () {
-//         Math.atan2(y, x);
-//     })
-//     .add('atan2', function () {
-//         atan2(y, x);
-//     })
-//     .add('approxAtan2', function () {
-//         approxAtan2(y, x);
-//     })
-//     .add('arctan2', function () {
-//         arctan2(y, x);
-//     })
-//     .on('cycle', function (event) {
-//         console.log(String(event.target));
-//     })
-//     .on('complete', function () {
-//         console.log('Fastest is ' + this.filter('fastest').map('name'));
-//     })
-//     .run();
-
+suite
+	.add('Math.atan2', function () {
+		Math.atan2(y, x);
+	})
+	.add('diamondAtan2', function () {
+		diamondAtan2(y, x);
+	})
+	.add('fastAtan2', function () {
+		fastAtan2(y, x);
+	})
+	.on('cycle', function (event) {
+		console.log(String(event.target));
+	})
+	.on('complete', function () {
+		console.log('Fastest is ' + this.filter('fastest').map('name'));
+	})
+	.run();
 
 // error < 0.07
-function atan2(y, x) {
-    var ax = Math.abs(x),
-        ay = Math.abs(y),
-        p = y / (ax + ay);
+// the accuracy is about 4 degrees
+function diamondAtan2(y, x) {
+	var ax = Math.abs(x),
+		ay = Math.abs(y),
+		p = y / (ax + ay);
 
-    if (x < 0) {
-        p = (y < 0 ? -2 : 2) - p;
-    }
-    // multiply by half of PI
-    return p * 1.5707963267948966;
+	if (x < 0) {
+		p = (y < 0 ? -2 : 2) - p;
+	}
+	// multiply by half of PI
+	return p * 1.5707963267948966;
 }
-
-
-var PI_FLOAT = 3.14159265;
-var PIBY2_FLOAT = 1.5707963;
 
 // error < 0.005
-function approxAtan2(y, x) {
-    if (x === 0) {
-        if (y > 0) return PIBY2_FLOAT;
-        if (y === 0) return 0;
-        return -PIBY2_FLOAT;
-    }
-    var atan,
-        z = y / x;
-    if (Math.abs(z) < 1) {
-        atan = z / (1 + 0.28 * z * z);
-        if (x < 0) {
-            if (y < 0) return atan - PI_FLOAT;
-            return atan + PI_FLOAT;
-        }
-    }
-    else {
-        atan = PIBY2_FLOAT - z / (z * z + 0.28);
-        if (y < 0) return atan - PI_FLOAT;
-    }
-    return atan;
+// the accuracy is about 0.3 degrees
+function fastAtan2(y, x) {
+	var pi = Math.PI;
+	if (x == 0) {
+		if (y > 0) return pi / 2;
+		if (y == 0) return 0;
+		return -pi / 2;
+	}
+
+	var atan, z = y / x;
+	if (z < -1 || z > 1) {
+		atan = pi / 2 - z / (z * z + 0.28);
+		if (y < 0) return atan - pi;
+	}
+	else {
+		atan = z / (1 + 0.28 * z * z);
+		if (x < 0) {
+			if (y < 0) return atan - pi;
+			return atan + pi;
+		}
+	}
+	return atan;
 }
-
-
-// error < 0.01
-function arctan2(y, x) {
-    var ONEQTR_PI = Math.PI / 4,
-        THRQTR_PI = 3 * ONEQTR_PI,
-        absY = Math.abs(y) + 1e-10,     // kludge to prevent 0/0 condition
-        angle,
-        r;
-
-    if (x < 0) {
-        r = (x + absY) / (absY - x);
-        angle = THRQTR_PI;
-    }
-    else {
-        r = (x - absY) / (x + absY);
-        angle = ONEQTR_PI;
-    }
-    angle += (0.1963 * r * r - 0.9817) * r;
-    if (y < 0) {
-        return (-angle);    // negate if in quad III or IV
-    } else {
-        return (angle);
-    }
-}
-
 
 function random(min, max) {
-    return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 }
 
-error(Math.atan2, atan2, function () {
-    return [
-        random(-20, 20),
-        random(-10, 10)
-    ]
-});
+// error(Math.atan2, diamondAtan2, function () {
+// 	return [
+// 		random(-2000, 2000),
+// 		random(-2000, 2000)
+// 	]
+// }, 10000);
 
-// error(Math.atan2, approxAtan2, function () {
+// error(Math.atan2, fastAtan2, function () {
 //     return [
-//         random(-20, 20),
-//         random(-10, 10)
+//         random(-2000, 2000),
+//         random(-2000, 2000)
 //     ]
-// });
-
-// error(Math.atan2, arctan2, function () {
-//     return [
-//         random(-20, 20),
-//         random(-10, 10)
-//     ]
-// });
+// }, 10000);
--- test/performance/error.js
+++ test/performance/error.js
@@ -1,11 +1,9 @@
-var count = 1000;
-
-module.exports = function (method1, method2, params) {
+module.exports = function (method1, method2, params, iterations) {
     var value1, value2, data, error, errorAbs, i;
     var sum = 0;
     var max = 0;
 
-    for (i = 0; i < count; i++) {
+    for (i = 0; i < iterations; i++) {
         data = params();
         value1 = method1.apply(null, data);
         value2 = method2.apply(null, data);
@@ -19,6 +17,6 @@ module.exports = function (method1, method2, params) {
         console.log((error > 0 ? ' ' : '') + error, value1, value2);
     }
     console.log('-----------------------------------------------');
-    console.log('error average: ' + sum / count);
+    console.log('error average: ' + sum / iterations);
     console.log('error max: ' + max);
 }
